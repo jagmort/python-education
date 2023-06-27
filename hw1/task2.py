c54fab160884abb73d59c625f87f267c0a4c5e76
Jag Mort <jagmort@gmail.com>
2023-06-27 06:55:48 +0300

    faster

--- hw1/task2.py
+++ hw1/task2.py
@@ -1,13 +1,21 @@
 from pathlib import Path
 
 import pandas as pd
-from sqlalchemy import create_engine, insert, MetaData, Table, Column, String
+from sqlalchemy import create_engine, MetaData, Table, Column, String
 
 from config import config
 
 
+def filter_fn(row):
+    try:
+        okved = row["data"]["СвОКВЭД"]["СвОКВЭДОсн"]["КодОКВЭД"]
+    except (KeyError, TypeError):
+        return False
+    else:
+        return okved.startswith("61")
+
 def main():
-    engine = create_engine(f'sqlite:///{config["db_file"]}', echo=True)
+    engine = create_engine(f'sqlite:///{config["db_file"]}')
     meta = MetaData()
     table = Table(
         config["telecom_table"], meta,
@@ -15,27 +23,38 @@ def main():
         Column('inn', String),
         Column('kpp', String), 
         Column('name', String), 
+        Column('full_name', String), 
         Column('okved', String), 
     )
     meta.create_all(engine)
 
     entries = Path(config["data_dir"])
+
     for i, file in enumerate(entries.glob('*.json')):
         print(i)
         companies = pd.read_json(file)
 
-        for row in companies.iterrows():
-            ogrn, inn, kpp, name  = row[1]["ogrn"], row[1]["inn"], row[1]["kpp"], row[1]["name"] 
-            try:
-                okved = row[1]["data"]["СвОКВЭД"]["СвОКВЭДДоп"]["КодОКВЭД"]
-            except (KeyError, TypeError):
-                okved = ""
-            else:
-                if okved.startswith("61"):
-                    stmt = insert(table).values(ogrn=ogrn, inn=inn, kpp=kpp, name=name, okved=okved).prefix_with('OR IGNORE')
-                    with engine.connect() as conn:
-                        conn.execute(stmt)
-                        conn.commit()
+        # filter only 61 OKVED
+        m = companies.apply(filter_fn, axis=1)
+        filtered = companies[m]
+
+        if not filtered.empty:
+            # create OKVED column
+            data = filtered["data"].apply(pd.Series)
+            svokved = data["СвОКВЭД"].apply(pd.Series)
+            svokvedosn = svokved["СвОКВЭДОсн"].apply(pd.Series)
+            okved = svokvedosn["КодОКВЭД"].apply(pd.Series)
+
+            # append and rename columns
+            final = pd.concat([filtered.drop(['data', 'data_version'], axis=1), okved], axis=1)
+            final.columns = ["ogrn", "inn", "kpp", "name", "full_name", "okved"]
+
+            final.to_sql(
+                config["telecom_table"], 
+                con=engine, 
+                index=False, 
+                if_exists='append'
+            )
 
 
 if __name__ == '__main__':
